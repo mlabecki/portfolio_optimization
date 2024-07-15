7caabc3c88fd9494b5ccd35b4fe9f719bd329d87
mlabecki <3i14159265@gmail.com>
2024-07-15 18:06:21 -0400

    Tested websites for tickers download

--- src/test_components_download.py
+++ src/test_components_download.py
@@ -1,29 +1,49 @@
 import pandas as pd
+import yfinance as yf
 import requests
-from bs4 import BeautifulSoup
 import json
 import os
 from convert_date import *
 
 s = requests.Session()
 
-# s.headers.update({
-#    "Accept-Language": "en-US,en;q=0.9", 
-#    "Accept-Encoding": "gzip, deflate, br",
-#    "User-Agent": "Java-http-client/"
-# })
-# r = s.get("https://www.nasdaq.com/market-activity/quotes/nasdaq-ndx-index")
-# soup = BeautifulSoup(r.content, "html.parser")
-# res = json.loads([x for x in soup.find("script", {"type": "application/json"})][0])
+max_tickers = 30
 
-# headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36"}
-
-headers = {'user-agent': 'Chrome/126.0.6478.127'}
+# user-agent from https://useragentstring.com/
+headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36'}
 res = s.get("https://api.nasdaq.com/api/quote/list-type/nasdaq100", headers=headers)
-print(res.headers)
+
 main_data = res.json()['data']['data']['rows']
 
-df_data = pd.DataFrame(columns=['Symbol', 'Company Name', 'Market Cap'])
-for i in range(len(main_data)):
-    md = main_data[i]
-    print(md['symbol'], md['companyName'], md['marketCap'])
+if res.status_code == 200:
+
+    main_data = res.json()['data']['data']['rows']
+    df_index = range(len(main_data))
+    df_final_index  = range(max_tickers)
+    
+    df_market_cap = pd.DataFrame(columns=['Symbol', 'Market Cap'], index=df_index)
+    df_final = pd.DataFrame(columns=['Symbol', 'Company Name', 'Market Cap'], index=df_final_index)
+
+    for i in df_index:
+    
+        md = main_data[i]
+        df_market_cap.loc[i, 'Symbol'] = md['symbol']
+        df_market_cap.loc[i, 'Market Cap'] = md['marketCap'].replace(',', '')
+    
+    df_market_cap['Market Cap'] = df_market_cap['Market Cap'].astype('Int64')
+    df_market_cap = df_market_cap.sort_values(by='Market Cap', ascending=False)
+    df_market_cap = df_market_cap.reset_index(drop=True)
+    df_market_cap = df_market_cap[: max_tickers]
+
+    df_final['Symbol'] = df_market_cap['Symbol']
+    df_final['Market Cap'] = df_market_cap['Market Cap']
+
+    for i in df_final_index:
+
+        ticker = df_final.loc[i, 'Symbol']
+        short_name = yf.Ticker(ticker).info['shortName']
+        long_name = yf.Ticker(ticker).info['longName']
+        company_name = short_name if len(short_name) <= len(long_name) else long_name
+        df_final.loc[i, 'Company Name'] = company_name
+        
+    print(df_final)
